049f5bc45cfbebfad35ce35a09ee7e02d6a38cc4
Duc-Thien-IT <ducthien0912.dev@gmail.com>
2024-09-18 16:13:56 +0700

    Log in - Log out - Sign in

---
+++ laravel-web-2/resources/views/admin/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin</title>
+</head>
+<body>
+    <h1>Hello Admin, Welcome to Dashboard</h1>
+    <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+</body>
+</html>
