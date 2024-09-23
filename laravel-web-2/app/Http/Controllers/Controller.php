2b75ebb0486e3aef9c741722ccf71906415cf2db
Duc-Thien-IT <ducthien0912.dev@gmail.com>
2024-09-23 23:16:44 +0700

    Update Swagger

--- laravel-web-2/app/Http/Controllers/Controller.php
+++ laravel-web-2/app/Http/Controllers/Controller.php
@@ -1,8 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
+
+#[
+    OA\Info(version: "1.0.0", description: "Fusion Center Document", title: "No title "),
+    OA\Server(url: 'http://127.0.0.1:8000/api', description: "local server"),
+    OA\Server(url: 'http://staging.example.com', description: "Staging server"),
+    OA\Server(url: 'http://example.com', description: "Product server"),
+    OA\SecurityScheme(securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header", scheme: "bearer"),
+]
 
 abstract class Controller
 {
-    //
+    
 }
--- laravel-web-2/app/Http/Controllers/HomeController.php
+++ laravel-web-2/app/Http/Controllers/HomeController.php
@@ -6,9 +6,18 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    /**
+     * @OA\GET(
+     *      path="/home",
+     *      tags={"home"},
+     *      summary="Get all Data",
+     *      description="Get list of all data",
+     *      @OA\Response(response=200, description="To retrieved successfully")
+     * )
+     */
+
     public function index()
     {
-
         return view('admin.index');
     }
 }
