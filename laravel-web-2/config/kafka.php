4e52ee961023f70fffe1e5182689906e10698263
Duc-Thien-IT <ducthien0912.dev@gmail.com>
2024-09-25 18:21:05 +0700

    Update DesignPattern for config kafka

---
+++ laravel-web-2/config/kafka.php
@@ -0,0 +1,18 @@
+<?php 
+
+return [
+    'brokers' => env('KAFKA_BROKERS', 'localhost:9092'),
+    'schema_registry' => env('KAFKA_SCHEMA_REGISTRY', 'http://localhost:8081'),
+    'topics' => [
+        'test' => env('KAFKA_TOPIC_TEST', 'test_topic'),
+    ],
+    'schema_avro' => [
+        'test' => env('KAFKA_SCHEMA_AVRO_TEST', 'test_schema'),
+    ],
+    'consumers' => [
+        'test' => env('KAFKA_CONSUMER_TEST', 'test_consumer'),
+    ],
+    'offset' => [
+        'test' => env('KAFKA_OFFSET_TEST', 'latest'),
+    ],
+];
